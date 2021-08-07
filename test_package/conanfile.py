19e61b6cc7eb97c820f4782e8524276bdf3902df
Mr-Slippery <badc0ded@gmail.com>
2021-08-07 22:05:20 +0200

    Add multi-threaded app using the compute stubs
    Added as a Conan test_package for convenience.
    
    test_package/program.thd is a sample interpreted program for the app.
    
    Signed-off-by: Mr-Slippery <badc0ded@gmail.com>

---
+++ test_package/conanfile.py
@@ -0,0 +1,29 @@
+import os
+
+from conans import ConanFile, CMake, tools
+
+
+class ComputeTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    build_requires = "gtest/1.8.1"
+
+    def build(self):
+        cmake = CMake(self)
+        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
+        # in "test_package"
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy('*.so*', dst='bin', src='lib')
+
+    def test(self):
+        if not tools.cross_building(self):
+            os.chdir("bin")
+            print("Running test binary...")
+            self.run(".%scompute_test" % os.sep)
+            print("Running demo with 3 threads...")
+            self.run(".%sthread_demo --threads 3 < ../../../program.thd" % os.sep)
